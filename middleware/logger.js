7216c7f7f39233cf0c1650e064d69be2a2b02a07
harshit <harshityadav0927@gmail.com>
2024-07-23 15:14:22 +0530

    v3

--- middleware/logger.js
+++ middleware/logger.js
@@ -2,3 +2,9 @@ const {format} = require('date-fns');
 const {v4: uuid}= require('uuid');
 const fs = require('fs');
 const fsPromises= require('fs').promises;
+const path = require('path');
+
+const logEvents = async(message, logEventname)=>{
+        const dateTime = format(new Date(),'dd-MM-yyyy\tHH:mm:ss')
+        const logItem = `${dateTime}\t${uuid()}\t${message}\n`
+}
