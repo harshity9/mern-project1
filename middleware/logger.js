006cd029163bda7cfefc8ddd86f1ceddf4cb8d3b
harshit <harshityadav0927@gmail.com>
2024-07-24 16:28:25 +0530

    v4

--- middleware/logger.js
+++ middleware/logger.js
@@ -7,7 +7,7 @@ const path = require('path');
 const logEvents = async(message, logFileName)=>{
         const dateTime = format(new Date(),'dd-MM-yyyy\tHH:mm:ss');
         const logItem = `${dateTime}\t${uuid()}\t${message}\n`;
-
+        
         try{
                 if(!fs.existsSync(path.join(__dirname, '..', 'logs'))){
                         await fsPromises.mkdir(path.join(__dirname, '..', 'logs'))
@@ -16,8 +16,7 @@ const logEvents = async(message, logFileName)=>{
         }
         catch (err){
                 console.log(err)
-        };
-        
+        };        
 }
 
 const logger = (req, res, next)=>{
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const path = require("path")
 const {logger} = require("./middleware/logger")
+const errorHandler = require("./middleware/errorHandler")
 const PORT =  process.env.PORT || 3500;
 
 app.use(logger);
@@ -13,5 +14,6 @@ app.all('*', (req,res)=>{
     res.sendFile(path.join(__dirname, 'views', '404.html'))
 })
 
+app.use(errorHandler);
 
 app.listen (PORT, ()=> console.log(`server running on ${PORT}`))
