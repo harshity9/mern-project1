b65fb7052e2fdfdefb080373b208f61092d19ecf
harshit <harshityadav0927@gmail.com>
2024-07-24 16:27:58 +0530

    v4

--- middleware/logger.js
+++ middleware/logger.js
@@ -4,7 +4,26 @@ const fs = require('fs');
 const fsPromises= require('fs').promises;
 const path = require('path');
 
-const logEvents = async(message, logEventname)=>{
-        const dateTime = format(new Date(),'dd-MM-yyyy\tHH:mm:ss')
-        const logItem = `${dateTime}\t${uuid()}\t${message}\n`
+const logEvents = async(message, logFileName)=>{
+        const dateTime = format(new Date(),'dd-MM-yyyy\tHH:mm:ss');
+        const logItem = `${dateTime}\t${uuid()}\t${message}\n`;
+
+        try{
+                if(!fs.existsSync(path.join(__dirname, '..', 'logs'))){
+                        await fsPromises.mkdir(path.join(__dirname, '..', 'logs'))
+                };
+                await fsPromises.appendFile(path.join(__dirname, '..', 'logs', logFileName), logItem)
+        }
+        catch (err){
+                console.log(err)
+        };
+        
 }
+
+const logger = (req, res, next)=>{
+        logEvents(`${req.method}\t${req.url}\t${req.headers.origin}`, 'reqLog.log')
+        console.log('logged')
+        next();
+}
+
+module.exports= {logEvents, logger};
